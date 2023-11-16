6c48f24f84f3115ff53d444351d0fb29bc5fe650
Ron Stoner <ron@team.casa>
2023-11-16 12:05:14 -0700

    Adding options and content type

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 window.onload = function() {
-    document.getElementById('protocol').addEventListener('change', generateCurlCommand);
+    document.getElementById('protocol').addEventListener('input', generateCurlCommand);
+    document.getElementById('method').addEventListener('input', generateCurlCommand);
     document.getElementById('url').addEventListener('input', generateCurlCommand);
+    document.getElementById('token').addEventListener('input', generateCurlCommand);
+    document.getElementById('contentType').addEventListener('input', generateCurlCommand);
     document.getElementById('headers').addEventListener('input', generateCurlCommand);
     document.getElementById('json').addEventListener('input', generateCurlCommand);
-    document.getElementById('token').addEventListener('input', generateCurlCommand);
 }
 
 function generateCurlCommand() {
     var protocol = document.getElementById('protocol').value;
+    var method = document.getElementById('method').value;
     var url = document.getElementById('url').value;
+    var token = document.getElementById('token').value;
+    var contentType = document.getElementById('contentType').value;
     var headers = document.getElementById('headers').value;
     var json = document.getElementById('json').value;
-    var token = document.getElementById('token').value;
 
-    var curlCommand = "curl -X GET ";
+    var curlCommand = "curl -X " + method + " ";
 
     // Add protocol
     curlCommand += protocol + "://";
 
     // Add URL
     curlCommand += url + " ";
+
+    // Add authorization token
+    if (token != '') {
+        curlCommand += "-H 'Authorization: Bearer " + token + "' ";
+    }
+
+    // Add Content-Type header
+    if (contentType != '') {
+        curlCommand += "-H 'Content-Type: " + contentType + "' ";
+    }
 
     // Add headers
     var headersArray = headers.split(',');
@@ -30,11 +44,6 @@ function generateCurlCommand() {
     // Add JSON payload
     if (json != '') {
         curlCommand += "-d '" + json + "' ";
-    }
-
-    // Add authorization token
-    if (token != '') {
-        curlCommand += "-H 'Authorization: Bearer " + token + "' ";
     }
 
     document.getElementById('curlCommand').innerText = curlCommand;
